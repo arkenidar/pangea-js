dceabddeeee40ec01209898134d40aebced33b01
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-13 23:34:23 +0200

    program2: another test program, toward more completeness

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-var code=`print 1 { print add 1 2 print "ciao+Dario!+1(+)2=3+using+((+))" }`
+var code1=`print 1 { print add 1 2 print "ciao+Dario!+1(+)2=3+using+((+))" }`
+var defs1={print:1, add:2}
+var program1={code:code1,defs:defs1,start:2}
 
+var code2=`{ ( def "factorial" 1
+if == 0 #1 1
+* #1 factorial + -1 #1 )
+( out factorial 3 ) }`
+var defs2={def:3,factorial:1,out:1,if:3,"==":2,"+":2,"*":2,"#1":0}
+var program2={code:code2,defs:defs2,start:0}
+// start: 1 -> 16 , 17 -> 5 , 0 -> 23 (1+16+5+1==23)
+
+var program=program2
+
+var code=program.code
 var words=parseCode(code)
-var defs={print:1, add:2}
+var defs=program.defs
 
-console.log(phraseLength(2), words)
+console.log("words:",words)
+console.log("phraseLength(wordIndex):",phraseLength(program.start))
 
 function phraseLength(wordIndex){
     var length=1
     var word=words[wordIndex]
+    if(word===undefined) console.log("wrong wordIndex:", wordIndex)
+    ///console.log(word,"(word)")
     function nextIndex(){ return wordIndex+length }
 
     if(isNumber(word)||isString(word)){}
@@ -23,7 +39,7 @@ function phraseLength(wordIndex){
         for(var i=0; i<argumentLength; i++)
             length+=phraseLength(nextIndex())
     
-    }else throw new Error("not handled: "+word)
+    }else { console.log(word,"(exception)") }
     return length
 }
 
@@ -36,7 +52,7 @@ function parse(text){
 }
 
 function parseCode(code){
-    words=code.split(" ")
+    words=code.split(/\s+/)
     words=words.map(handlePlus)    
     return words
 }
